78118710f356beed951fe89b8f2d383f9f5df580
S L <lordstepheng@gmail.com>
2018-03-03 07:51:55 +0000

    automatically backed up by learn

---
+++ addon.rb
@@ -0,0 +1,4 @@
+letters = ["b","a","c"]
+puts "The first letter in the alphabet is: #{letters[1]}"
+puts "The second letter in the alphabet is: #{letters[0]}"
+puts "The third letter in the alphabet is: #{letters[2]}"
